bd3950aaf3f1f334ed709f5f5687c515ddab2699
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2019-12-12 20:57:39 -0300

    esta bem no inicio ainda pois faz tempo que não programo em JAVA, mas vai sair, ou ao menos vai passar desse ponto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+	Salvar oi = new Salvar();
+	//char matriz[][]  = new char[4][4];
+        int m[][] = new int[10][10];
+        for(int i = 0; i < 10; i++){
+	    for(int j = 0; j < 10; j++){
+	        m[i][j] = i;
+	        //matriz[i][j] = (char) i;
+        }
+    }
+	//oi.teste(matriz);
     }
 }
